4e77d9073a5376fcec3e7ada7bb78a901260d945
Nora Tomas <noratomas@hotmail.com>
2018-03-01 09:37:31 +0100

    added forms

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,5 +1,11 @@
 from ecommerce import settings
 from django.shortcuts import render, get_object_or_404, HttpResponse, Http404
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import UserForm
+from django.template import loader
+
 
 
 def search(request):
@@ -13,3 +19,23 @@ def search(request):
         if query:
             context['message'] = query
     return render(request, "ecommerce/search.html", context=context)
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'registration.html'
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form: ': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+
+            #Cleaned (normalized) data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password()
+            user.save()
