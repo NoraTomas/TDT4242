3c3a91a34dec2f8b7b03a9c9955e0b5fcfb364f1
Nora Tomas <noratomas@hotmail.com>
2018-03-08 11:33:59 +0100

    changed settings to work

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -119,14 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/home/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
