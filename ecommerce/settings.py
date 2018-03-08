539fc06c9df7cc08ea63a956913a24b509f919b4
Nora Tomas <noratomas@hotmail.com>
2018-03-08 11:29:24 +0100

    fixed settings error

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/home/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
