43646b1d0092c03751d54df2a7b6d2ad7d24fb52
Nora Tomas <noratomas@hotmail.com>
2018-03-18 12:52:47 +0100

    changed settings to fit postgresql database

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tdt4242',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres314',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
